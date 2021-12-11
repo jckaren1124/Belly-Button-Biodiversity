747c34454dd05a44eabc874a89f3113ed96afb1f
Karen Kan <jckaren1124@hotmail.com>
2021-12-10 20:29:59 -0800

    reorganized folder structures and added challenge

--- Module/data-12-4-3_d3.json/sample_dataset.js
+++ Module/data-12-4-3_d3.json/sample_dataset.js
@@ -14,7 +14,12 @@ function init() {
   
   init();
   
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+  //  buildCharts(newSample);
+    }
 
+//create buildMetadata() function to populate the city/state from demographic info panel
   function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
@@ -23,12 +28,10 @@ function init() {
       var PANEL = d3.select("#sample-metadata");
   
       PANEL.html("");
-      PANEL.append("h6").text(result.location);
+      PANEL.append("h6").console.log(result);
     });
   }
 
-
-function optionChanged(newSample) {
-  buildMetadata(newSample);
-  buildCharts(newSample);
-  }
+//Skill Drill 12.4.3
+//modify buildMetadata() function to populate all the demographic data from demographic info panel
+// 
